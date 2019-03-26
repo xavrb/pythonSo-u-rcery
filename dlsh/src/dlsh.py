0e80bf0331bab2ba075a0c869c1d1be7819a512f
Javier Mejia <javier.mejia@protonmail.ch>
2019-03-26 18:16:10 +0100

    deletes properly on a fixed path and png or jpg

--- dlsh/src/dlsh.py
+++ dlsh/src/dlsh.py
@@ -1,9 +1,24 @@
+# a simple script to delete screenshots  (images as it recognizes png and jpg)
+
 import os
 
-
+nukedFiles = 0
+pngs = 0
+jpgs =0
 print("Script to delete screenshots.\n ")
+noFiles = len(os.listdir('..\\testing\\Screenshots Work'))
+print("Number of files to nuke: " + str(noFiles) + " >:) ")
+
 for filesc in os.listdir('..\\testing\\Screenshots Work'):
-    print ("Deleting: " + str(filesc) + "\t>>\n")
-    if filesc.endswith('.png'):
-        print("Found a *png file! >:) -- Nuking it!")
+    print (" >> Nuking: " + str(filesc))
+    if filesc.endswith('.png') or filesc.endswith('.jpg'):
+        if filesc.endswith('.png'): pngs = pngs + 1
+        if filesc.endswith('.jpg'): jpgs = jpgs + 1
+        nukedFiles = nukedFiles + 1
         os.remove('..\\testing\\Screenshots Work\\' + filesc)
+
+
+print("Log of nuked files: " + str(nukedFiles))
+print("JPGS nuked: " + str(jpgs))
+print("PNGS nuked: " + str(pngs))
+print("Delete them from your paper bin too!")
