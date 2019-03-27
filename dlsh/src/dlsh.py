c857e604f6138357e339738601bb3927fd6a367a
Javier Mejia <javier.mejia@protonmail.ch>
2019-03-27 11:39:56 -0400

    now takes a path as argument

--- dlsh/src/dlsh.py
+++ dlsh/src/dlsh.py
@@ -1,21 +1,22 @@
 # a simple script to delete screenshots  (images as it recognizes png and jpg)
 
 import os
-
+import sys
+targetPath = sys.argv[1]
 nukedFiles = 0
 pngs = 0
 jpgs =0
 print("Script to delete screenshots.\n ")
-noFiles = len(os.listdir('..\\testing\\Screenshots Work'))
+noFiles = len(os.listdir(targetPath))
 print("Number of files to nuke: " + str(noFiles) + " >:) ")
 
-for filesc in os.listdir('..\\testing\\Screenshots Work'):
+for filesc in os.listdir(targetPath):
     print (" >> Nuking: " + str(filesc))
     if filesc.endswith('.png') or filesc.endswith('.jpg'):
         if filesc.endswith('.png'): pngs = pngs + 1
         if filesc.endswith('.jpg'): jpgs = jpgs + 1
         nukedFiles = nukedFiles + 1
-        os.remove('..\\testing\\Screenshots Work\\' + filesc)
+        os.remove(targetPath +"/" + filesc)
 
 
 print("Log of nuked files: " + str(nukedFiles))
