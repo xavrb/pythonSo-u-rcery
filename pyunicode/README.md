56834ce6e28b318fcb1b5e87dc795e3de983cf7f
Javier Mejia <javier.mejia@protonmail.ch>
2019-04-03 12:14:30 -0400

    added pyunicode in super short syntax

--- pyunicode/README.md
+++ pyunicode/README.md
@@ -4,4 +4,9 @@
 A simple script to translate from UTF8 encoding to escape entities that can bet printed directly by a System.out.println() in Java or other lang.  
 
 
+## Usage
+
+´´´  
+python3 pyunicode.py  "Klüft skräms inför på fédéral électoral große"
+´´´
 
--- pyunicode/src/pyunicode.py
+++ pyunicode/src/pyunicode.py
@@ -1,12 +1,8 @@
 # -*- coding: utf-8 -*-
 import json
 import sys 
-#unicodes = "Klüft skräms inför på fédéral électoral große"
 
 
-
-unicodes = sys.argv[1]
-escapech = str(json.dump(unicodes, sys.stdout))
-print(escapech)
+print(str(json.dump(sys.argv[1], sys.stdout)))
 
 
